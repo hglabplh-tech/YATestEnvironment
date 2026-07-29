05a0a3203fed7b8abf21a356a66cfb312cabdd0b
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2026-07-29 14:33:59 +0200

    added raw values in the let- to overcome lazy eval in the wrong context

--- src/main/clojure/io/github/hglabplh_tech/reflect/clojure/active_data/parse_meta.clj
+++ src/main/clojure/io/github/hglabplh_tech/reflect/clojure/active_data/parse_meta.clj
@@ -10,7 +10,7 @@
          result []]
     (if (empty? the-rest)
       result
-      (let [the-result  (fun (doall (first the-rest)))]
+      (let [the-result (fun (doall (first the-rest)))]
         (recur (rest the-rest) (conj result the-result)))
       )
     ))
@@ -23,8 +23,8 @@
     (if (or (rinspect/realm? value) (is-a? rinspect/function-case value))
       (let [realm-base (if (rinspect/realm? value)
                          {:description   (rinspect/description value)
-                        :predicate-fun (rinspect/predicate value)
-                        :meta-data     (rinspect/metadata value)}
+                          :predicate-fun (rinspect/predicate value)
+                          :meta-data     (rinspect/metadata value)}
                          {})]
         (cond
           (rinspect/builtin-scalar? value)
@@ -40,30 +40,34 @@
           (let [int-from-realm-raw (rinspect/integer-from-to-realm-from value)
                 int-to-realm-raw (rinspect/integer-from-to-realm-to value)
                 cooked {:int-from-val (select-meta-rec int-from-realm-raw)
-                        :int-to-val    (select-meta-rec int-to-realm-raw)}]
+                        :int-to-val   (select-meta-rec int-to-realm-raw)}]
             (println "realm integer from to")
             [realm-base cooked]
             )
 
           (rinspect/real-range? value)
-          (let [cooked {:clusive-left  (rinspect/real-range-realm-clusive-left value)
-                        :left-limit    (rinspect/real-range-realm-left value)
-                        :right-limit   (rinspect/real-range-realm-right value)
-                        :clusice-right (rinspect/real-range-realm-clusive-right value)}]
+          (let [realm-clusive-left-raw (rinspect/real-range-realm-clusive-left value)
+                realm-left-raw (rinspect/real-range-realm-clusive-left value)
+                realm-clusive-right (rinspect/real-range-realm-clusive-right value)
+                realm-right-raw (rinspect/real-range-realm-right value)
+                cooked {:clusive-left  realm-clusive-left-raw
+                        :left-limit    realm-left-raw
+                        :clusice-right realm-clusive-right
+                        :right-limit   realm-right-raw}]
             (println "realm real-range")
             [realm-base cooked]
             )
 
           (rinspect/union? value)
           (let [the-rec (rinspect/union-realm-realms value)
-                cooked {:union-realm-def (map select-meta-rec the-rec)}]
+                cooked {:union-realm-def (walk-through select-meta-rec the-rec)}]
             (println "realm union")
             [realm-base cooked])
 
           (rinspect/intersection? value)
           (let [the-rec (rinspect/intersection-realm-realms value)
                 cooked {:intersect-realm-def
-                        (map select-meta-rec the-rec)}]
+                        (walk-through select-meta-rec the-rec)}]
             (println "realm intersection")
             [realm-base cooked])
 
@@ -85,56 +89,65 @@
             )
 
           (rinspect/map-with-keys? value)
-          (let [cooked {:map-key-realm (select-meta-rec
-                                         (rinspect/map-with-keys-realm-map value))}] ;; look how we have to get  it -> compound ?
+          (let [keys-realm-map-raw (rinspect/map-with-keys-realm-map value)
+                cooked {:map-key-realm (select-meta-rec keys-realm-map-raw)}]
             (println "realm map with keys")
             [realm-base cooked]
             )
 
           (rinspect/map-of? value)
-          (let [cooked {:map-entry-realms [(select-meta-rec
-                                             (rinspect/map-of-realm-key-realm value))
-                                           (select-meta-rec
-                                             (rinspect/map-of-realm-value-realm value))]
-                        }]
+          (let [key-realm-raw (rinspect/map-of-realm-key-realm value)
+                value-realm-raw (rinspect/map-of-realm-value-realm value)
+                cooked {:map-entry-realms [(select-meta-rec key-realm-raw)
+                                           (select-meta-rec value-realm-raw)]}]
             ;; look how we have to get  it -> compound ?
             (println "map of realm")
             [realm-base cooked]
             )
 
-          ;; here the rest is implemented
+          (rinspect/optional? value)
+          (let [raw (rinspect/optional-realm-realm value)
+                cooked (select-meta-rec raw)]
+            (println "optional realm found: ")
+            (pprint cooked)
+            [realm-base cooked]
+            )
 
           (rinspect/enum? value)
-          (let [cooked {:enum-def (rinspect/enum-realm-values value)}]
+          (let [raw (rinspect/enum-realm-values value)
+                cooked {:enum-def raw}]
             (println "enum realm")
             [realm-base cooked]
             )
 
           (rinspect/tuple? value)
-          (let [cooked {:tuple-realms
-                        (walk-through select-meta-rec
-                             (rinspect/tuple-realm-realms value))}]
+          (let [tuple-realms-raw (rinspect/tuple-realm-realms value)
+                cooked {:tuple-realms (walk-through select-meta-rec tuple-realms-raw)}]
             (println "realm tuple")
             [realm-base cooked]
             )
 
           (rrec/is-a? rinspect/record-realm-field value)
-          (let [cooked {:field-def [(rinspect/record-realm-field-name value)
-                                      (rinspect/record-realm-field-realm value)
-                                    (rinspect/record-realm-field-getter value)
+          (let [field-name-raw (rinspect/record-realm-field-name value)
+                field-realm-raw (rinspect/record-realm-field-realm value)
+                field-getter-raw (rinspect/record-realm-field-getter value)
+                cooked {:field-def [field-name-raw
+                                    (select-meta-rec field-realm-raw)
+                                    field-getter-raw
                                     ]}]
             (println "realm field")
             [realm-base cooked]
             )
 
           (rinspect/record? value)
-          (let [cooked {:record-name
-                        (rinspect/record-realm-name value)
-                        :record-ctor
-                        (rinspect/record-realm-constructor value)
+          (let [name-raw (rinspect/record-realm-name value)
+                ctor-raw (rinspect/record-realm-constructor value)
+                fields-raw (rinspect/record-realm-fields value)
+                cooked {:record-name name-raw
+                        :record-ctor ctor-raw
                         :rec-fields
                         (walk-through (fn [v] (println "rec-field ->") (pprint v) (select-meta-rec v))
-                             (rinspect/record-realm-fields value))}]
+                                      fields-raw)}]
             (println "record  realm")
             [realm-base cooked]
             )
@@ -144,24 +157,24 @@
           (let [dummy []]
             (println "function case entered ->")
             (let [position-args-raw (rinspect/function-case-positional-argument-realms value)
-                pos-args-def (walk-through (fn [v]
-                                                    (println "pos arguments:")
-                                                    (pprint v)
-                                                    (select-meta-rec v)) position-args-raw)
-                opt-args-raw (rinspect/function-case-optional-arguments-realm value)
-                opt-args-def (if (nil? opt-args-raw)
-                               []
-                               (select-meta-rec opt-args-raw))
-                ret-val-raw   (rinspect/function-case-return-realm value)
-                ret-val-def  (select-meta-rec ret-val-raw)
-                cooked {:function-case-def {:pos-arg-def pos-args-def
-                                            :opt-arg-def opt-args-def
-                                            :ret-val-def ret-val-def
-                                            }}]
+                  pos-args-def (walk-through (fn [v]
+                                               (println "pos arguments:")
+                                               (pprint v)
+                                               (select-meta-rec v)) position-args-raw)
+                  opt-args-raw (rinspect/function-case-optional-arguments-realm value)
+                  opt-args-def (if (nil? opt-args-raw)
+                                 []
+                                 (select-meta-rec opt-args-raw))
+                  ret-val-raw (rinspect/function-case-return-realm value)
+                  ret-val-def (select-meta-rec ret-val-raw)
+                  cooked {:function-case-def {:pos-arg-def pos-args-def
+                                              :opt-arg-def opt-args-def
+                                              :ret-val-def ret-val-def
+                                              }}]
 
-            (println "function case realm")
-            [realm-base cooked]
-            ))
+              (println "function case realm")
+              [realm-base cooked]
+              ))
 
           (rinspect/function? value)
           (let [the-cases (rinspect/function-realm-cases value)
@@ -174,20 +187,20 @@
             )
 
           (rinspect/delayed? value)
-          (let [cooked {:delayed-realm-def (select-meta-rec
-                                             (rinspect/delayed-realm-delay value))}]
+          (let [raw (rinspect/delayed-realm-delay value)
+                cooked {:delayed-realm-def (select-meta-rec raw)}]
             (println "delayed realm")
             [realm-base cooked]
             )
           (rinspect/named? value)
-          (let [cooked {:named-realm-def
-                        {:name       (rinspect/named-realm-name value)
-                         :name-realm (select-meta-rec
-                                       (rinspect/named-realm-realm value))}}]
+          (let [name-raw (rinspect/named-realm-name value)
+                realm-raw (rinspect/named-realm-realm value)
+                cooked {:named-realm-def
+                        {:name       name-raw
+                         :name-realm (select-meta-rec realm-raw)}}]
             (pprint cooked)
             (println "named realm")
-            [realm-base cooked])
-          )
+            [realm-base cooked]))
         (do
           (throw (IllegalArgumentException. (str "invalid realm member " value)))
           )))))
@@ -195,7 +208,7 @@
 
 (clojure.core/defn get-data-active-meta [meta-data]
   (let [active-meta (get meta-data attach/fn-realm-meta-key)
-        a-meta (map select-meta-rec active-meta)
+        a-meta (walk-through select-meta-rec active-meta)
         stripped-meta-desc (first a-meta)
         the-value (first (rest (first stripped-meta-desc)))]
     (pprint active-meta)
