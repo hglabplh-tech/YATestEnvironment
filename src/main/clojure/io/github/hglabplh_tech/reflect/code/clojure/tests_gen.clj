0057f4afe3cbb79379cd6a3fb16fc47e00ff4447
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2026-08-07 03:30:42 +0200

    check in to be safe

--- src/main/clojure/io/github/hglabplh_tech/reflect/code/clojure/tests_gen.clj
+++ src/main/clojure/io/github/hglabplh_tech/reflect/code/clojure/tests_gen.clj
@@ -1,1 +1,9 @@
 (ns io.github.hglabplh-tech.reflect.code.clojure.tests-gen)
+
+
+(defn extract-fun-def [fun-def-case]
+  (let [fdef fun-def-case
+        freturn (get fdef :ret-val-def)
+        fposargs (get fdef :pos-arg-def)
+        foptargs (get fdef :opt-arg-def)]
+    ))
